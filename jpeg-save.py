fca104e0a0e662a4880ee6dce4a2e804c5462eea
Lauri Hakko <lauri.hakko@gmail.com>
2013-11-26 22:08:05 +0200

    made argument descriptions more consistent

--- jpeg-save.py
+++ jpeg-save.py
@@ -6,11 +6,11 @@ import os
 
 parser = argparse.ArgumentParser(description="Resave jpeg files multiple times")
 parser.add_argument("-n", "--number", default=500, type=int,
-                    help="Number of resaves, default=500")
+        help="Number of resaves, default: 500")
 parser.add_argument("-q", "--quality", default=95, type=int,
-                    help="Image quality between 1 and 95, default 95")
+        help="Image quality between 1 and 95, default: 95")
 parser.add_argument("-k", "--keep", action="store_true",
-                    help="Keep all intermediate files, default: no")
+        help="Keep all intermediate files, default: no")
 parser.add_argument("filename") 
 
 args = parser.parse_args()
