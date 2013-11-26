34b67738a9351cc9e0661f3f7d7ac9613169e958
Lauri Hakko <lauri.hakko@gmail.com>
2013-11-26 21:39:31 +0200

    made argument parsing

--- jpeg-save.py
+++ jpeg-save.py
@@ -4,7 +4,15 @@ from PIL import Image
 import argparse
 
 parser = argparse.ArgumentParser(description="Resave jpeg files multiple times")
-parser.add_argument("-n", action="store", nargs=1, default=500, type=int,
-                    help="How many times to resave, default=500")
+parser.add_argument("-n", "--number", default=500, type=int,
+                    help="Number of resaves, default=500")
+parser.add_argument("-q", "--quality", default=95, type=int,
+                    help="Image quality between 1 and 95, default 95")
 parser.add_argument("filename") 
+
+args = parser.parse_args()
+
+if args.quality < 1 or args.quality > 95:
+    print("Error: Quality should be between 1 and 95")
+    exit(1)
 
